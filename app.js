d924793cadcda22429471b0402e81c9139d2bd5b
lachendeKatze <govkn@yahoo.com>
2016-09-29 22:19:36 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 /* conduit between the html ui and the hexiwear */
 let bleConnectButton = document.querySelector('#bleConnectButton');
 
-var bleConnect(){
+function bleConnect(){
     hexiWeather.connect()
     .then(() => console.log('connected'))
     .catch(error => { console.log('connect error!');
     });
 };
+
+/** function bleConnect(buttonID) {
+  var thisButton = document.getElementById(buttonID);
+  thisButton.setAttribute("fill-opacity",0.9);
+  led.connect()
+      .then(() => console.log('connected'))
+      .catch(error => { console.log('connect error!'); });
+  
+};
+**/
