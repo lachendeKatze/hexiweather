e6a09638e6bc454da7a134237ecf0040db79e70b
lachendeKatze <govkn@yahoo.com>
2016-09-29 22:27:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ function bleConnect(){
     });
 };
 
+bleConnectButton.addEventListener('click',function(){
+    hexiWeather.connect()
+    .then(() => console.log('connected'))
+    .catch(error => { console.log('connect error!');
+    });
+};    
+
+/*
+bleSwitch.addEventListener('click',function(){
+  console.log('new switch click, connect');
+  relayClick.connect()
+      .then(() => console.log('connected'))
+      .catch(error => { console.log('connect error!');
+    });
+
+**/
+
+
 /** function bleConnect(buttonID) {
   var thisButton = document.getElementById(buttonID);
   thisButton.setAttribute("fill-opacity",0.9);
