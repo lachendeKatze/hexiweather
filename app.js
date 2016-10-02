e12a2e28b9b6f065465cfb92e6a714a31498bfc8
lachendeKatze <govkn@yahoo.com>
2016-10-02 00:10:54 -0400

    beginning to develop

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 /* conduit between the html ui and the hexiwear */
 let bleConnectButton = document.querySelector('#bleConnectButton');
+let temperatureButton = document.querySelector('#tempButton');
+let pressureButton = document.querySelector('#presButton');
+let humidityButton = document.querySelector('#humidButton');
 
 function bleConnect(){
     hexiWeather.connect()
@@ -15,6 +18,32 @@ bleConnectButton.addEventListener('click',function(){
     .catch(error => { console.log('connect error!');
     });
 });    
+
+temperatureButton.addEventListener('click',function(){
+    tempReading = hexiWeather._readCharacteristic(hexiWeather.temperature);
+    alert('temp: ' + tempReading);
+    });
+});
+
+pressureButton.addEventListener('click',function(){
+    pressureReading = hexiWeather._readCharacteristic(hexiWeather.pressure);
+    alert('pres: ' + pressureReading);
+    });
+});
+
+humidityButton.addEventListener('click',function(){
+    humidityReading = hexiWeather._readCharacteristic(hexiWeather.humidity);
+    alert('humid: ' + humidityReading);
+    });
+});
+
+
+
+
+
+
+
+
 
 /*
 bleSwitch.addEventListener('click',function(){
